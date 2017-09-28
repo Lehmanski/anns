cab4dafc394a6045f2909ae7dd574d321375a027
NilusvanEdel <ledelkott@uos.de>
2017-09-28 16:11:22 +0200

    simplified it

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def single_cnn(input):
         kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu)
-
+    '''
     # Pooling Layer #1
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[4, 4], strides=2)
     # Droput Layer #1
@@ -43,11 +43,13 @@ def single_cnn(input):
     # Pooling Layer #2
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
     return pool2
+    '''
+    return conv1
 
 
-def combine_layer(inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7):
+def combine_layer(inp0, inp1 ): #,inp2, inp3, inp4, inp5, inp6, inp7
     ''' combine the 8 layers to one single concatinated layer'''
-    comb_layer = tf.concat([inp0,inp1,inp2,inp3, inp4, inp5, inp6, inp7], 0)
+    comb_layer = tf.concat([inp0,inp1], 0) #, inp2,inp3, inp4, inp5, inp6, inp7
     return comb_layer
 
 
@@ -70,6 +72,7 @@ def comb_conv(input):
         training=False,
         name=None
     )
+    ''''
     # Convolutional Layer #2
     conv2 = tf.layers.conv2d(
         inputs=dropout1,
@@ -99,7 +102,15 @@ def comb_conv(input):
     flattened = tf.reshape(dropout2, [-1, tmp])
     # Dense Layer # 1
     logits = tf.layers.dense(inputs=flattened, units=262144, activation=tf.nn.relu)
+    return logits7
+    '''
+    tmp = np.prod(dropout1.shape.as_list()[1:])
+    flattened = tf.reshape(dropout1, [-1, tmp])
+    # Dense Layer # 1
+    dense_layer = tf.layers.dense(inputs=flattened, units=65536, activation=tf.nn.relu)
+    logits = tf.layers.dense(inputs=dense_layer, units=262144)
     return logits
+
 
 def weight_variable(shape):
     """weight_variable generates a weight variable of a given shape."""
@@ -147,35 +158,37 @@ if __name__ == "__main__":
     data, test_data = get_data()
     x_0 = tf.placeholder(tf.float32, [None, 518400])
     x_1 = tf.placeholder(tf.float32, [None, 518400])
+    ''''
     x_2 = tf.placeholder(tf.float32, [None, 518400])
     x_3 = tf.placeholder(tf.float32, [None, 518400])
     x_4 = tf.placeholder(tf.float32, [None, 518400])
     x_5 = tf.placeholder(tf.float32, [None, 518400])
     x_6 = tf.placeholder(tf.float32, [None, 518400])
     x_7 = tf.placeholder(tf.float32, [None, 518400])
-    # TO DO CHANGE
+    '''''# TO DO CHANGE
     # Define loss and optimizer
     # 64 * 64 * 64
     y_ = tf.placeholder(tf.float32, [None, 262144])
     # the CNN for each image invidually
     conv_0 = single_cnn(x_0)
     conv_1 = single_cnn(x_1)
+    '''
     conv_2 = single_cnn(x_2)
     conv_3 = single_cnn(x_3)
     conv_4 = single_cnn(x_4)
     conv_5 = single_cnn(x_5)
     conv_6 = single_cnn(x_6)
     conv_7 = single_cnn(x_7)
+    '''
     print(conv_0.get_shape())
     # the concatenation of each layer
-    conc_layer = combine_layer(conv_0, conv_1, conv_2, conv_3, conv_4, conv_5, conv_6, conv_7)
+    conc_layer = combine_layer(conv_0, conv_1) #, conv_2, conv_3, conv_4, conv_5, conv_6, conv_7
     print(conc_layer.get_shape())
     logits = comb_conv(conc_layer)
 
     ##################
     with tf.name_scope('loss'):
-        cross_entropy = loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=logits)
-        cross_entropy = tf.reduce_mean(cross_entropy)
+        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=logits)
         print(cross_entropy)
 
         with tf.name_scope('adam_optimizer'):
@@ -189,13 +202,13 @@ if __name__ == "__main__":
             train_step.run(feed_dict={
                 x_0: batch[0][0],
                 x_1: batch[0][1],
-                x_2: batch[0][2],
+                y_: batch[1]})
+            '''x_2: batch[0][2],
                 x_3: batch[0][3],
                 x_4: batch[0][4],
                 x_5: batch[0][5],
                 x_6: batch[0][6],
-                x_7: batch[0][7],
-                y_: batch[1]})
+                x_7: batch[0][7],'''
 
     batch = get_batch(data, batch_size)
     print()
